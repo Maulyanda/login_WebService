b135d0c27d232e7f6e8e653b2858b6568fc88902
Maulyanda <maulyanda94.kcb@gmail.com>
2017-06-05 21:21:36 +0700

    Service Login

---
+++ Service.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+
+public class Service : System.Web.Services.WebService
+{
+    public Service()
+    {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string HelloWorld()
+    {
+        return "Hello World";
+    }
+
+    [WebMethod]
+    public Boolean login(string username, string password)
+    {
+        string user, pass;
+        user = "admin";
+        pass = "1234";
+        if (username == user && password == pass)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
